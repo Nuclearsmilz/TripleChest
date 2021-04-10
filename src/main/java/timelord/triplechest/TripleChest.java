90e5e0f05a366812112e685c244bef1507260850
Jon Marien <nuclearsmilz@gmail.com>
2021-04-10 03:37:12 -0400

    Added all necessary basic mod things, and for some reason i set the project sdk to java 16 so this might just be poopoo

---
+++ src/main/java/timelord/triplechest/TripleChest.java
@@ -0,0 +1,10 @@
+package timelord.triplechest;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TripleChest implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("YOOO THOSE ARE SOME THICC A** CCCHESTS!");
+	}
+}
